57b9ca9a5366b4aec389260168807454d9b32e4f
Sebastian Kacprzak <nait@naitbit.com>
2014-10-13 16:05:14 +0200

    Removed: Support for Async Retrofit call with callbacks, since event matching was bugged.

--- src/main/java/com/byoutline/ottocachedfield/OttoCachedField.java
+++ src/main/java/com/byoutline/ottocachedfield/OttoCachedField.java
@@ -6,7 +6,6 @@ import com.byoutline.cachedfield.SuccessListener;
 import com.byoutline.eventcallback.ResponseEvent;
 import com.byoutline.ottocachedfield.internal.OttoErrorListener;
 import com.byoutline.ottocachedfield.internal.OttoSuccessListener;
-import com.byoutline.ottocachedfield.internal.RetofitValueProvider;
 import com.byoutline.ottoeventcallback.PostFromAnyThreadIBus;
 import com.squareup.otto.Bus;
 import javax.inject.Provider;
@@ -19,30 +18,29 @@ public class OttoCachedField<T> extends CachedFieldImpl<T> {
 
     public static Provider<String> defaultSessionIdProvider;
     public static Bus defaultBus;
-    public static long MAX_WAIT_TIME_IN_S = 60;
 
-    public OttoCachedField(RetrofitCall<T> valueGetter, ResponseEvent<T> successEvent) {
+    public OttoCachedField(Provider<T> valueGetter, ResponseEvent<T> successEvent) {
         this(valueGetter, successEvent, null);
     }
 
-    public OttoCachedField(RetrofitCall<T> valueGetter, ResponseEvent<T> successEvent, Object errorEvent) {
+    public OttoCachedField(Provider<T> valueGetter, ResponseEvent<T> successEvent, Object errorEvent) {
         this(defaultSessionIdProvider, valueGetter, successEvent, errorEvent, defaultBus);
     }
 
-    public OttoCachedField(Provider<String> sessionIdProvider, RetrofitCall<T> valueGetter, ResponseEvent<T> successEvent, Object errorEvent, Bus bus) {
+    public OttoCachedField(Provider<String> sessionIdProvider, Provider<T> valueGetter, ResponseEvent<T> successEvent, Object errorEvent, Bus bus) {
         this(sessionIdProvider, valueGetter, successEvent, errorEvent, new PostFromAnyThreadIBus(bus));
     }
 
-    private OttoCachedField(Provider<String> sessionIdProvider, RetrofitCall<T> valueGetter, ResponseEvent<T> successEvent, Object errorEvent, PostFromAnyThreadIBus bus) {
+    private OttoCachedField(Provider<String> sessionIdProvider, Provider<T> valueGetter, ResponseEvent<T> successEvent, Object errorEvent, PostFromAnyThreadIBus bus) {
         this(sessionIdProvider,
-                new RetofitValueProvider(valueGetter, bus, sessionIdProvider),
+                valueGetter,
                 new OttoSuccessListener(bus, successEvent),
                 new OttoErrorListener(bus, errorEvent),
                 bus);
     }
 
     private OttoCachedField(Provider<String> sessionProvider,
-            RetofitValueProvider<T> valueGetter,
+            Provider<T> valueGetter,
             SuccessListener<T> successHandler, ErrorListener errorHandler,
             PostFromAnyThreadIBus bus) {
         super(sessionProvider, valueGetter, successHandler, errorHandler);
