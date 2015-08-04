4778075dca1591d52139af1ad6148e1a381d8a01
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-08-04 15:56:43 +0200

    Javadoc: different wording of class javadoc of events.

--- src/main/java/com/byoutline/ottocachedfield/events/ResponseEventWithArgImpl.java
+++ src/main/java/com/byoutline/ottocachedfield/events/ResponseEventWithArgImpl.java
@@ -3,7 +3,7 @@ package com.byoutline.ottocachedfield.events;
 /**
  * Simple implementation of {@link ResponseEventWithArg} that contains only getters and setter.
  * Moved to {@link com.byoutline.ibuscachedfield.events.ResponseEventWithArgImpl}.
- * This copy was kept only for backward compatibility.
+ * This class is a copy that was kept only for backward compatibility.
  *
  * @param <RETURN_TYPE> Type of response response
  * @param <ARG_TYPE>    Type of argument used to receive/calculate response
